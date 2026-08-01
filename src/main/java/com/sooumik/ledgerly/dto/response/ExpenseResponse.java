c113556a62e0a0e5dcfb8cb25b9b96f748153cc1
Soumik2426 <soumikmaity2426@gmail.com>
2026-08-01 20:48:16 +0530

    Add whole response and request DTOs

--- src/main/java/com/sooumik/ledgerly/dto/response/ExpenseResponse.java
+++ src/main/java/com/sooumik/ledgerly/dto/response/ExpenseResponse.java
@@ -1,7 +1,14 @@
 package com.sooumik.ledgerly.dto.response;
 
+import lombok.*;
+
 import java.time.LocalDate;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ExpenseResponse {
     private Long id;
     private String title;
