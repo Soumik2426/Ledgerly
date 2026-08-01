bb160645bd8360cc879a0241e94c1e1dde968b8f
Soumik2426 <soumikmaity2426@gmail.com>
2026-08-01 20:44:45 +0530

    Implement the controller for the endpoints

---
+++ src/main/java/com/sooumik/ledgerly/controller/ExpenseController.java
@@ -0,0 +1,11 @@
+package com.sooumik.ledgerly.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/expenses")
+@RequiredArgsConstructor
+public class ExpenseController {
+}
