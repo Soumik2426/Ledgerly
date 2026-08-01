842f5672b2c6f764b2a1d6a95122735a4cb03735
Soumik2426 <soumikmaity2426@gmail.com>
2026-08-01 23:09:07 +0530

    Add Standard API response throughout all the endpoints

--- src/main/java/com/sooumik/ledgerly/advice/ApiError.java
+++ src/main/java/com/sooumik/ledgerly/advice/ApiError.java
@@ -1,4 +1,18 @@
 package com.sooumik.ledgerly.advice;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ApiError {
+    private Integer status;
+    private String message;
+    private List<String> subErrors;
 }
--- src/main/java/com/sooumik/ledgerly/advice/ApiResponse.java
+++ src/main/java/com/sooumik/ledgerly/advice/ApiResponse.java
@@ -1,4 +1,21 @@
 package com.sooumik.ledgerly.advice;
 
-public class ApiResponse {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse<T> {
+    @Builder.Default
+    private LocalDateTime timestamp = LocalDateTime.now();
+
+    private T data;
+    private String message;
+    private ApiError error;
 }
